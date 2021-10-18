f7babd545942bd8ab9c872a7c204c41aea65a8e2
nithunkarur <rknithun@gmail.com>
2021-10-18 13:40:26 +0530

    RENAMED

--- Fibinoci.java
+++ Fibinoci.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class fibinoci{
+class Fibinoci{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the no to find the fibinoci");
--- PerfectNo.java
+++ PerfectNo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class perfectNo{
+class PerfectNo{
 		public static void main(String[] args){
 			Scanner sc=new Scanner(System.in);
 			System.out.println("Enter the no");
